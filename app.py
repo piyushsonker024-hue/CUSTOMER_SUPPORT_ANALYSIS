4df6e4ea76d07624203fedf516baa7ca1cc731de
Piyush Sonker <piyushsonker024@gmail.com>
2026-07-30 16:02:23 +0530

    minor fix

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
         " SLA Analysis",
         " Agent Analysis",
         " Customer Insights",
-        " data Quality"
+        " Data Quality",
         " Weekly KPI"
     ]
 )
